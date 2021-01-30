4688567cb258aedf468bb80d13877c39c0af5597
BushraWaheed <72761774+bushrawaheed@users.noreply.github.com>
2021-01-30 18:34:00 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,59 @@
+let weather = {};
+console.log(weather);
+
+weather = {
+  temp: 13,
+  humidity: "90%"
+};
+console.log(weather);
+
+console.log(weather.temp);
+console.log(weather.humidity);
+
+weather = {
+  temp: 13,
+  humidity: "90%",
+  windspeed: "11k/h"
+};
+console.log(weather);
+
+let weathers = [
+  {
+    temp: 13,
+    humidity: "90%",
+    windspeed: "11k/h"
+  }
+];
+console.log(weathers);
+
+let forecast = [
+  {
+    day: "Monday",
+    temp: 13
+  },
+  {
+    day: "Tuesday",
+    temp: 13
+  },
+  {
+    day: "Wednesday",
+    temp: 13
+  },
+  {
+    day: "Thursday",
+    temp: 13
+  },
+  {
+    day: "Friday",
+    temp: 13
+  },
+  {
+    day: "Saturday",
+    temp: 13
+  },
+  {
+    day: "Sunday",
+    temp: 13
+  }
+];
+console.log(forecast);
